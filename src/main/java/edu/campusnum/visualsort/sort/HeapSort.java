65d89ac6bc1a93dd83d6941d3640403c45c212a9
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-08-01 14:43:51 +0200

    heap check

--- src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/HeapSort.java
@@ -11,6 +11,38 @@ import edu.campusnum.visualsort.model.ObservableArray;
 public class HeapSort implements SortAlgorithm{
     @Override
     public void sort(ObservableArray array) {
-        throw new UnsupportedOperationException("Not implemented");
+        organiser(array);
+        for (int i=array.getLength()-1;i>=0;i--){
+            array.swap(0,i);
+            redescendre(array,i,0);
+        }
+    }
+
+    void organiser(ObservableArray array){
+        for (int i = 0; i < array.getLength()-1; i++) {
+            remonter(array, i);
+        }
+    }
+    void remonter(ObservableArray array, int index){
+        if (array.get(index)>array.get(index/2)){
+            array.swap(index,index/2);
+            remonter(array,index/2);
+        }
+    }
+    void redescendre (ObservableArray array,int element,int index){
+        int formule = 2*index+1;
+        if (formule<element){
+            int max;
+            if (array.get(formule)>array.get(2*index)){
+                max = formule;
+            }else{
+                max = 2*index;
+            }
+            if (array.get(max)>array.get(index)){
+                array.swap(max,index);
+                redescendre(array,element,max);
+            }
+        }
+
     }
 }
