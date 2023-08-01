18a24a4b9673e8d99183f8830f9a10a1b64b37bf
Nicolas <nicolas.hureau@le-campus-numerique.fr>
2023-08-01 16:45:29 +0200

    quick check

--- src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
+++ src/main/java/edu/campusnum/visualsort/sort/QuickSort.java
@@ -11,6 +11,26 @@ import edu.campusnum.visualsort.model.ObservableArray;
 public class QuickSort implements SortAlgorithm{
     @Override
     public void sort(ObservableArray array) {
-        throw new UnsupportedOperationException("Not implemented");
+        rapidSort(array, 0,array.getLength()-1 );
+    }
+    int part (ObservableArray array,int first, int last,int pivot){
+        array.swap(pivot, last);
+        int j = first;
+        for (int i=first;i<=last-1;i++){
+            if (array.get(i)<= array.get(last)){
+                array.swap(i,j);
+                j++;
+            }
+        }
+        array.swap(last,j);
+        return j;
+    }
+    void rapidSort(ObservableArray array, int first, int last){
+        if (first<last){
+            int pivot = first;
+            pivot = part(array,first,last,pivot);
+            rapidSort(array,first,pivot-1);
+            rapidSort(array,pivot+1,last);
+        }
     }
 }
